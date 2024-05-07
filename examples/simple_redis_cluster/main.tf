61e0d9b62babdd6ae6c5a5542af6870a82330050
Shriram Rajaraman <shriram.rajaraman@hashicorp.com>
2024-05-07 12:41:26 +0530

    Updated the domain id'

--- examples/simple_redis_cluster/main.tf
+++ examples/simple_redis_cluster/main.tf
@@ -10,7 +10,7 @@ module "redshift_cluster" {
   version = "~>  0.1.0"
 
   aws_account                 = data.aws_caller_identity.current.account_id
-  datazone_domain_name        = coalesce(var.datazone_domain_id, data.tfe_outputs.domain.values.domain_id)
+  datazone_domain_id          = coalesce(var.datazone_domain_id, data.tfe_outputs.domain.values.domain_id)
   datazone_project_id         = coalesce(var.datazone_project_id, data.tfe_outputs.domain.values.environment_project_id)
   region                      = var.region
 }
